96bf77bb8819733ec6b4c1e953bfd7e7480dbc5c
Vitaly Domnikov <dotcypress@gmail.com>
2016-12-24 00:27:17 -0800

    commnd+c command+v

--- app/extractors/vc.ru.js
+++ app/extractors/vc.ru.js
@@ -1,10 +1,10 @@
 const { Extractor } = require('./../utils')
 
-const tjournal = new Extractor((url) => url.startsWith('https://vc.ru'))
+const vc = new Extractor((url) => url.startsWith('https://vc.ru'))
 
-tjournal.title(($) => $('.b-article__head h1').text())
-tjournal.authorName(($) => $('.b-article__infopanel__author a').text())
-tjournal.authorUrl(($, url) => url)
-tjournal.content(($) => $('.b-article__intro p, article p, article img, article h2, article h3, article h4, article h5'))
+vc.title(($) => $('.b-article__head h1').text())
+vc.authorName(($) => $('.b-article__infopanel__author a').text())
+vc.authorUrl(($, url) => url)
+vc.content(($) => $('.b-article__intro p, article p, article img, article h2, article h3, article h4, article h5'))
 
-module.exports = tjournal
+module.exports = vc
